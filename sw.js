d876918c0b0676bea57a807dda7a7f74a36a4c87
nabe-chan-1973 <keichan0322@gmail.com>
2026-03-19 10:58:17 +0900

    Initial commit: 日々の実践 PWA app

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE = 'daily-practice-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+];
+
+self.addEventListener('install', (evt) => {
+  evt.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (evt) => {
+  evt.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (evt) => {
+  // Only handle GET requests
+  if (evt.request.method !== 'GET') return;
+
+  evt.respondWith(
+    caches.match(evt.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(evt.request).then((res) => {
+        // Cache same-origin requests
+        if (res && res.status === 200 && evt.request.url.startsWith(self.location.origin)) {
+          const clone = res.clone();
+          caches.open(CACHE).then((c) => c.put(evt.request, clone));
+        }
+        return res;
+      });
+    }).catch(() => caches.match('./index.html'))
+  );
+});
